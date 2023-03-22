7c01b1826d9e65337b93578b84f84bc65cfb7c38
Newbiecodeweaver <carlosgilino@gmail.com>
2023-03-22 18:38:07 +0100

    define IndexController with showIndex to return index products view

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class IndexController extends Controller
+{
+    public function showIndex() {
+
+        $response = Http::get('http://127.0.0.1:8000/api/products/');
+
+        $products = json_decode($response);
+
+        return view('index', compact('products'));
+
+    }
+}
