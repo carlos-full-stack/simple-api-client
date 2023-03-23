bbfebf717d2af3485f1c33e2edee85aa7dcbdbef
Newbiecodeweaver <carlosgilino@gmail.com>
2023-03-23 18:26:08 +0100

    add api client endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [IndexController::class, 'showIndex'])->name('index');;
+
+Route::get('/new', [ProductController::class, 'newProduct'])->name('product.new');
+
+Route::post('/new', [ProductController::class, 'saveProduct'])->name('product.save');
+
+Route::get('/product/{id}', [ProductController::class, 'showProduct'])->name('product.show');
+
+Route::get('/product/edit/{id}', [ProductController::class, 'editProduct'])->name('product.edit');
+
+Route::put('/product/update/{id}', [ProductController::class, 'updateProduct'])->name('product.update');
+
+Route::delete('/delete/{id}', [ProductController::class, 'deleteProduct'])->name('product.delete');
